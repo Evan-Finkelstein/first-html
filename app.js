1a07770663d41f2be7a2d90473ed26638ce09501
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-09-02 17:38:17 -0700

    color buttons

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const myName = document.getElementById('name');
 const myInput = document.getElementById('name-input');
 const myButton = document.getElementById('name-button');
+const blueButton = document.getElementById('blue-button')
+const greenButton = document.getElementById('green-button')
+const pinkButton = document.getElementById('pink-button')
 
 myButton.addEventListener('click', () => {
     const userInput = myInput.value;
     myName.textContent = userInput;  
-    console.log(userInput)
 });
+blueButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = "blue"
+})
+greenButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = "green"
+})
+pinkButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = "palevioletred"
+})
